8050f6a7b44fdf2a7de93ae49f54872800cdf645
yashsngh44 <lhhammer44@gmail.com>
2026-06-18 03:29:09 +0530

    Create RansomNote.cpp

---
+++ Week_2/Day_1/RansomNote.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+
+        vector<int> freq(26, 0);
+
+        for(char ch : magazine)
+            freq[ch - 'a']++;
+
+        for(char ch : ransomNote){
+            if(freq[ch - 'a'] == 0)
+                return false;
+
+            freq[ch - 'a']--;
+        }
+
+        return true;
+    }
+};

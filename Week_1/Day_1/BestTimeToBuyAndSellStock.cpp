e4c4f9400fd7717c73a3a28cd56313dc1af7eb1f
yashsngh44 <lhhammer44@gmail.com>
2026-06-18 03:11:15 +0530

    Create BestTimeToBuyAndSellStock.cpp

---
+++ Week_1/Day_1/BestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minPrice = INT_MAX;
+        int profit = 0;
+
+        for(int price : prices){
+            minPrice = min(minPrice, price);
+            profit = max(profit, price - minPrice);
+        }
+
+        return profit;
+    }
+};
